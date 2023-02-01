7418ccd842eee0d43b3fa910d5c3748053597851
Leandro <leandrolupano@hotmail.com>
2023-02-01 19:18:11 +0100

    Add Pet type and class

---
+++ src/models/Pet.ts
@@ -0,0 +1,21 @@
+import { PetStructure } from './PetsStructure';
+
+export class Pet implements PetStructure {
+  public id: string;
+  public isAdopted: boolean;
+
+  static generateId() {
+    const aNumbers = new Uint32Array(1);
+    crypto.getRandomValues(aNumbers);
+    return ('000000' + aNumbers[0]).slice(-6);
+  }
+
+  constructor(
+    public name: string,
+    public specie: string,
+    public owner: string
+  ) {
+    this.id = Pet.generateId();
+    this.isAdopted = false;
+  }
+}
